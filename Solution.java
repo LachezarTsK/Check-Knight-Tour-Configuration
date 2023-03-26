7e885e63bf099ae42dc0f1f4d79ded7d74c44221
Lachezar Kutsarov <lkutsarov@gmail.com>
2023-03-26 12:30:24 +0200

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,44 @@
+
+public class Solution {
+
+    private record Point(int row, int column) {}
+    private int rows;
+    private int columns;
+
+    public boolean checkValidGrid(int[][] grid) {
+        if (grid[0][0] != 0) {
+            return false;
+        }
+
+        rows = grid.length;
+        columns = grid[0].length;
+
+        Point[] coordinatesConsecutiveMoves = new Point[rows * columns];
+        fillArrayCoordinatesConsecutiveMoves(grid, coordinatesConsecutiveMoves);
+
+        return eachConsecutiveMovesIsValid(coordinatesConsecutiveMoves);
+    }
+
+    private boolean eachConsecutiveMovesIsValid(Point[] coordinatesConsecutiveMoves) {
+        for (int i = 1; i < coordinatesConsecutiveMoves.length; ++i) {
+            if (!isValidKnightMove(coordinatesConsecutiveMoves[i - 1], coordinatesConsecutiveMoves[i])) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private void fillArrayCoordinatesConsecutiveMoves(int[][] grid, Point[] coordinatesConsecutiveMoves) {
+        for (int r = 0; r < rows; ++r) {
+            for (int c = 0; c < columns; ++c) {
+                int indexMove = grid[r][c];
+                coordinatesConsecutiveMoves[indexMove] = new Point(r, c);
+            }
+        }
+    }
+
+    private boolean isValidKnightMove(Point first, Point second) {
+        return (Math.abs(first.row - second.row) == 1 && Math.abs(first.column - second.column) == 2)
+                || (Math.abs(first.row - second.row) == 2 && Math.abs(first.column - second.column) == 1);
+    }
+}
